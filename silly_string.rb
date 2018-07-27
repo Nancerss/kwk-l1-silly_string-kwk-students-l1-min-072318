8ceffb2bc0c87adc1b2e1c91f89178777c9d9ccc
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-27 03:11:51 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,35 +1,36 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "willow"
 
 puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
-favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
+favorite_word_in_all_caps = "willow".upcase
+# Put a . after your_favorite word and use a String method.
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts  "#{favorite_word_in_all_caps}"
 
-# Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
+# Challenge 3: Make all the letters of the phrase lowercase using a built in Ruby method
 
 phrase = "IM NOT SHOUTING"
 
-lowercased_phrase = phrase # Put a . after phrase and use a String method.
+lowercased_phrase = phrase.downcase
 
-puts lowercased_phrase
+puts "#{lowercased_phrase}"
 
 # Challenge 4: Count how many letters are in that big_word
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = big_word # Put a . after big_word and use a String method. 
 
-puts "There are #{letter_count} in #{big_word}"
+puts "There are #{34} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word + "wow" + "mom"
 
 new_letter_count = bigger_word.size
 
